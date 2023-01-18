63c92a63aa8f38b3f6306c7ff66a2cfb1941ded4
DanielHealy <danielhealyhosting3@gmail.com>
2023-01-18 19:22:14 +0000

    mongo is async and writes to correct db

--- api/index.cjs
+++ api/index.cjs
@@ -4,9 +4,6 @@ const cors = require('cors')
 const transaction = require('./models/transaction.cjs')
 require('dotenv').config()
 
-
-console.log(process.env.VITE_MONGO_URL)
-
 const app = express();
 // const mongoURL = import.meta.env.VITE_MONGO_URL;
 
@@ -19,12 +16,12 @@ app.get('/api/test', (req, res) => {
 	res.json('test ok');
 });
 
-app.post('/api/transaction/', (req, res) => {
-	// mongoose.connect(mongoURL)
-	console.log(mongoURL)
-	res.json(req.body)
-	console.log(req.body)
-	// const {expense, datetime, desc} = res.body
+app.post('/api/transaction/', async (req, res) => {
+	await mongoose.connect(process.env.VITE_MONGO_URL);
+	// console.log(req.body)
+	const { expense, datetime, desc } = req.body
+	const transactionCreate = await transaction.create({ expense, datetime, desc })
+	res.json(transactionCreate);
 })
 
 
